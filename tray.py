852a08145a870113a97ce3a66c959d06aa1a0ede
Goosople <goosople@goosople.top>
2025-04-08 23:26:23 +0800

    Implement basic GUI

--- tray.py
+++ tray.py
@@ -1,8 +1,23 @@
-#!python3
-
+#!/usr/bin/env python3
+import os
 import sys
-from PyQt6.QtWidgets import QApplication, QSystemTrayIcon, QMenu
+from PyQt6.QtWidgets import (
+    QApplication,
+    QSystemTrayIcon,
+    QMenu,
+    QMessageBox,
+)
 from PyQt6.QtGui import QAction, QIcon
+
+DEVICE_PATH = os.getenv(
+    "NOTOUCHSCREEN_DEVICE_PATH", "/sys/bus/hid/drivers/hid-multitouch"
+)
+
+global device
+
+
+def _(s):
+    return s  # TODO: a placeholder for future localization
 
 
 class TrayIcon(QSystemTrayIcon):
@@ -12,7 +27,7 @@ class TrayIcon(QSystemTrayIcon):
 
         self.menu = QMenu()
 
-        action_quit = QAction("", self.menu)
+        action_quit = QAction("Quit", self.menu)
         action_quit.triggered.connect(self.on_quit)
         self.menu.addAction(action_quit)
 
@@ -22,15 +37,47 @@ class TrayIcon(QSystemTrayIcon):
 
     def on_tray_click(self, reason):
         if reason == QSystemTrayIcon.ActivationReason.Trigger:
-            print("")  # switch touch screen
+            switch_touchscreen(device)
 
     def on_quit(self):
         QApplication.quit()
 
 
+def show_error_and_exit(err):
+    QMessageBox(
+        QMessageBox.Icon.Critical,
+        _("Error"),
+        err,
+        QMessageBox.StandardButton.Close,
+    ).exec()
+    QApplication.quit()
+
+
+def get_devices():
+    try:
+        return list(
+            filter(
+                lambda f: os.path.isdir(os.path.join(DEVICE_PATH, f)) and ":" in f,
+                os.listdir(DEVICE_PATH),
+            )
+        )
+    except FileNotFoundError:
+        show_error_and_exit(_("DEVICE_PATH is not found"))
+    except PermissionError:
+        show_error_and_exit(_("Permission denied to get devices"))
+    except Exception as e:
+        show_error_and_exit(_("Unknown error") + f": {e}")
+
+
+def switch_touchscreen(device):
+    print(device)
+    return  # FIXME: implement
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     app.setQuitOnLastWindowClosed(False)
+    device = get_devices()[0]  # TODO: add selection
     tray = TrayIcon()
     tray.show()
     sys.exit(app.exec())
